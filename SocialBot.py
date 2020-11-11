0d4c158ac9df070a60dd0e17ad6ddd6e93177357
Arighna <riju23chakra@gmail.com>
2020-11-11 21:32:12 +0530

    scroll mechanics

--- SocialBot.py
+++ SocialBot.py
@@ -19,11 +19,28 @@ class SocialBot:
         email = bot.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div[1]/div/form/div/div[1]/div/label/div/div[2]/div/input')
         password = bot.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div[1]/div/form/div/div[2]/div/label/div/div[2]/div/input')
         log = bot.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div[1]/div/form/div/div[3]/div/div/span/span')
+        email.clear()
         email.send_keys(self.username)
+        password.clear()
         password.send_keys(self.pw)
         log.click()
         time.sleep(5)
 
+    def like_tweet(self, hashtag):
+        bot = self.bot
 
-twitter = SocialBot('<username>', '<password>')
+        bot.get('https://www.twitter.com/search?q='+ hashtag +'&src=typed_query')
+        time.sleep(3)
+
+        """
+        the range() determines how many times the bot scrolls...
+        by default it's 10 times...
+        """
+        for i in range(1, 10):
+            bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+            time.sleep(2)
+
+
+twitter = SocialBot('RijuStone', 'Ferrorist@859075')
 twitter.login()
+twitter.like_tweet('spiderman')
